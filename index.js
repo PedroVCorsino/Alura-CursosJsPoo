b2d22ad6f54a2725e5a13f3e0abf8293a5b8a8a4
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2021-10-11 20:45:35 -0300

    atributos privados

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ class Cliente {
 
 class ContaCorrente{
     agencia;
-    saldo;
+    #saldo = 0; // O "#" torna esse atributo privado. 
 
     sacar(valor) { 
-        if(this.saldo >= valor){
-            this.saldo -=valor;
-        }
+        if(this.#saldo < valor) return; 
+        
+        this.#saldo -=valor;
+        return valor;
+    }
+
+    depositar(valor) {
+        if (valor <= 0) return
+        
+        this.#saldo +=valor;
+        return valor;
+
+    }
+
+    getSaldo(){
+        return this.#saldo;
     }
 }
 
@@ -18,22 +31,10 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 11122233301;
-
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
-
-const contaCorrenteAlice = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
-contaCorrenteRicardo.agencia = 1002;
-
-
-
-contaCorrenteRicardo.saldo = 1555;
+contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.sacar(50)
-console.log(contaCorrenteRicardo.saldo);
 
+console.log(contaCorrenteRicardo.getSaldo())
