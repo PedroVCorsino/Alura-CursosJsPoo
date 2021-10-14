645fee49b4461672d77b3aa50af70de80769a1b8
Pedro Vinicius <pedro.vinicius@acai.com.br>
2021-10-14 11:25:09 -0300

    Polimorfismo

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ import { Gerente } from "./funcionario/gerente.js";
 import { Diretor } from "./funcionario/diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
+const cliente = new Cliente("Lais", 12345678902, "123");
+
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
 diretor.cadastrarSenha('123456789');
 
 const gerente = new Gerente("Ricardo", 5000, 12345678901);
 gerente.cadastrarSenha('123456');
 
-const estaLogado = SistemaAutenticacao.login(gerente, "123456");
-console.log(estaLogado);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789");
+console.log(diretorEstaLogado);
+
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123456");
+console.log(gerenteEstaLogado);
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "123");
+console.log(clienteEstaLogado);
+
